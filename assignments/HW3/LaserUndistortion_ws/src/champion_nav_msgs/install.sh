b4ba8570cbe216126834bc10d26aa60bd3d58ad9
peng00bo00 <peng00bo00@github.com>
2020-12-15 13:22:14 +0400

    add files

--- assignments/HW3/LaserUndistortion_ws/src/champion_nav_msgs/install.sh
+++ assignments/HW3/LaserUndistortion_ws/src/champion_nav_msgs/install.sh
@@ -7,5 +7,5 @@ ip=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | gre
 export ROS_MASTER_URI="http://$ip:11311" #网卡ip
 
 export ROS_HOSTNAME=$ip
-source /opt/ros/kinetic/setup.bash
-catkin_make -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/ros/kinetic install
+source /opt/ros/melodic/setup.bash
+catkin_make -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/ros/melodic install
