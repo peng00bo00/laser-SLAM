becf6583e9387a9b8320893ba1e6a9d55baa38ce
peng00bo00 <pengbo_tongji@126.com>
2021-01-05 19:08:50 +0400

    Update main.cpp

--- assignments/HW6/LSSLAMProject/src/ls_slam_gtsam/src/main.cpp
+++ assignments/HW6/LSSLAMProject/src/ls_slam_gtsam/src/main.cpp
@@ -8,7 +8,7 @@
 #include <gtsam/inference/Key.h>
 #include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
-#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/nonlinear/Marginals.h>
 #include <gtsam/nonlinear/Values.h>
 
